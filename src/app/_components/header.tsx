d2ba7e0f59f0e4af3e82533c3a9287efe9153abd
ololade <belloololade2003@gmail.com>
2025-01-05 15:41:26 +0100

    routing in next js

---
+++ src/app/_components/header.tsx
@@ -0,0 +1,14 @@
+export default function Header(){
+    return (
+      <>
+        <header className="navbar">
+          <h1>LOGO</h1>
+          <nav>
+            <a href="/home">HOME</a>
+            <a href="/about">ABOUT</a>
+            <a href="/profile">PROFILE</a>
+          </nav>
+        </header>
+      </>
+    );
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+function Barchart(){
+    return <h1>Bar Chart</h1>
+}
+
+export default function Dashboard(){
+    return (
+      <>
+        <h1>Dashboard</h1>
+      </>
+    );
+}
